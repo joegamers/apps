632ae093b800df82f852ded5e36d765c4057e872
joegamers <joel22sd@gmail.com>
2023-10-13 13:44:10 -0400

    Mi sitio web

---
+++ pix_mail/config.php
@@ -0,0 +1,68 @@
+<?php
+	$mail_type = 'ce';
+	//-----------------------------------------------------------------------------------------
+    $to_Email       = 'joel22sd@gmail.com'; //Replace with recipient email address
+    $subject        = 'Tú app Android'; //Subject line for emails
+
+    // your recaptcha secret key
+    $secret = '';      // Add your reCAPTCHA secret key
+    //-----------------------------------------------------------------------------------------
+
+    // Language
+    $language = 'EN';
+
+    /* Mailchimp setting */
+    define('MC_APIKEY', ''); // Your API key from here - http://admin.mailchimp.com/account/api
+    define('MC_LISTID', ''); // List unique id from here - http://admin.mailchimp.com/lists/
+
+    /* Campaign Monitor setting. */
+    define('CM_APIKEY', ''); // Your APIKEY from here - https://pixfort.createsend.com/admin/account/
+    define('CM_LISTID', ''); // List ID from here - https://www.campaignmonitor.com/api/getting-started/#listid
+
+    /* GetResponse setting. To enable a setting, uncomment (remove the '#' at the start of the line)*/
+    define('GR_APIKEY', ''); // Your API key from here - https://app.getresponse.com/my_api_key.html
+    define('GR_CAMPAIGN', ''); // Campaign name from here - https://app.getresponse.com/campaign_list.html
+
+    /* AWeber setting */
+    define('AW_AUTHCODE', ''); // Your Authcode from here - https://auth.aweber.com/1.0/oauth/authorize_app/647b2efd
+    define('AW_LISTNAME', ''); // List name from here - https://www.aweber.com/users/autoresponder/manage
+
+    /* ActiveCampaign setting */
+    define('ACTIVECAMPAIGN_URL', ''); // API_URL : Go to My Settings -> Developers
+    define('ACTIVECAMPAIGN_API_KEY', ''); // API_KEY : Go to My Settings -> Developers
+    define('list_id', ''); // API_KEY : Go to My Settings -> Developers
+
+    /* MailerLite setting */
+    define('MailerLite_API_KEY', ''); // API Key: Go to https://app.mailerlite.com/integrations/api/
+    define('MailerLite_LIST_ID', ''); // LIST ID (GroupID): Go to https://app.mailerlite.com/integrations/api/
+
+    /* FreshMail setting */
+    define ( 'FM_API_KEY', '' ); // API Key: Go to https://app.freshmail.com/en/settings/integration/
+    define ( 'FM_API_SECRET', '' ); // API Secret: Go to https://app.freshmail.com/en/settings/integration/
+    define ( 'FM_list_id', '' ); // List's API key: Go to list -> Parameters
+
+    /* Sendloop setting */
+    define('Sendloop_API3_KEY', '');
+    define('Sendloop_SUBDOMAIN', '');
+
+
+	/* Sendy setting */
+    define('Sendy_URL', ''); // Your Sendy installation URL (without trailing slash).
+    define('Sendy_apikey', ''); // Your API key. Available in Sendy Settings.
+	define('Sendy_listId', '');
+
+	/* MailWizz setting */
+    define('Mailwizz_apiUrl', ''); // API URL, Should be http://www.yourdomain.com/api/index.php (where MailWizz is installed)
+    define('Mailwizz_publicKey', ''); // Public Key: Go to "Api Keys" and create new key
+	define('Mailwizz_privateKey', ''); // Private Key: Go to "Api Keys" and get the private key
+
+    /* Hubspot setting */
+    define('Hubspot_api', '');
+	define('Hubspot_list', '');
+
+    /* iContact setting */
+    define('iContact_appId', '');
+    define('iContact_apiPassword', '');
+    define('iContact_apiUsername', '');
+	define('iContact_list', '');
+?>
